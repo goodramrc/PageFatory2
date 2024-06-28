a8f3c70afd1162cb1923c7160a012ac9223f8eb5
mariusrc <maryusrosca@gmail.com>
2024-06-28 18:22:15 +0300

    curs 33 Page Factory

---
+++ src/main/java/selenium/utils/BaseTest.java
@@ -0,0 +1,41 @@
+package selenium.utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+
+    public static WebDriver driver;
+    public BasePage app;
+
+    @Parameters({"appUrl"})
+    @BeforeClass(alwaysRun = true)
+    public void setup(String url) {
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
+
+        app = new BasePage(driver);
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(5000);//bad practice
+        //driver.close();//inchide tab-ul curent
+        driver.quit();//inchide fereastra de browser
+
+    }
+
+}
